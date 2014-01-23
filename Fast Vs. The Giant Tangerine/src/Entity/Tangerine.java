973f2ef6bd97f4439e4fbcc2e3bf44fbeb8c9843
josh sparks <j80mar4@gmail.com>
2014-01-23 08:59:28 -0700

    tangerine basics

---
+++ Fast Vs. The Giant Tangerine/src/Entity/Tangerine.java
@@ -0,0 +1,60 @@
+package Entity;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+import TileMap.TileMap;
+
+public class Tangerine extends Enemy{
+
+	private BufferedImage[] sprites;
+
+	public Tangerine(TileMap tm, int speed){
+		super(tm);
+
+		moveSpeed = speed - 2;
+		maxSpeed = speed - 2;
+		fallSpeed = .1;
+		maxFallSpeed = 4.0;
+
+		width = 118;
+		cwidth = 118;
+		height = 658;	
+		cheight = 658;	
+
+		health = maxHealth = 1;
+		
+		try{
+			BufferedImage spritesheet = ImageIO.read(getClass().getResourceAsStream("/Sprites/Tangerine.png"));
+			sprites = new BufferedImage[1];
+			for(int i = 0; i < sprites.length; i++){
+				sprites[i] = spritesheet.getSubimage(i * width, 0, width, height);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		animation = new Animation();
+		animation.setFrames(sprites);
+		animation.setDelay(100);
+		
+		facingRight = true;
+	}
+
+	public void getNextPosition(){
+
+	}
+
+	public void update(){
+		setPosition(xtemp, ytemp);
+
+		animation.update();
+
+	}
+
+	public void draw(Graphics2D g){
+		setMapPosition();
+		super.draw(g);
+	}
+}
